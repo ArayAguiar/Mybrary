4b97ef3c1e455717a1c3e63f15764ddfbd6e67dd
ArayAguiar <arayaguiar@hotmail.com>
2022-12-16 15:06:43 +0100

    Initial Author Routes

--- server.js
+++ server.js
@@ -4,18 +4,20 @@ if(process.env.NODE_ENV !== 'production') {
     
 }
 
-
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded ({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }) //? might delete the last part
@@ -25,11 +27,14 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
 
 
 
-//brew services start mongodb-community@5.0  to start using mongodb-community
-//
-//brew services stop mongodb-community@5.0 to stop when you finish
+// to start using mongodb-community server use the following
+// " brew services start mongodb-community@5.0 " 
+
+// to stop use the following
+// " brew services stop mongodb-community@5.0 "
